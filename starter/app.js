00af055b150cab3f1a2756be198d54f9cb45f850
GodwinEgo <godwinehile04@gmail.com>
2023-04-10 00:44:08 +0100

    methods

--- starter/app.js
+++ starter/app.js
@@ -8,6 +8,13 @@ app.get( '/hello', ( req, res ) =>
   res.send( 'Task Manager App' )
 } )
 
+// app.get('/api/v1/tasks')   - get all tasks
+// app.post('api/v1/tasks')   - create a new task
+// app.get('api/v1/tasks/:id') - get single task
+// app.patch('api/v1/tasks/:id') - update task
+// app.delete('api/v1/tasks/:id') - delete task
+
+
 const port = 3000
 
 
